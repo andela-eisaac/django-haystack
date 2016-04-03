eac3216aa482d8c07cf968cb6e2efbbcc3966675
andela-eisaac <deejecks@live.com>
2016-04-03 12:25:31 +0100

    Remove unneeded parentheses

--- haystack/fields.py
+++ haystack/fields.py
@@ -441,7 +441,7 @@ class FacetField(SearchField):
 
         if 'facet_for' in kwargs:
             self.facet_for = kwargs['facet_for']
-            del(kwargs['facet_for'])
+            del kwargs['facet_for']
 
         return kwargs
 
